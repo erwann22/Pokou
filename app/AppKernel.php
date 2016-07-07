1f290489e7110a998ae48721cedb22881d85693f
erwann <erwann.leprioux@gmail.com>
2016-07-08 00:12:59 +0200

    modifications AppKernel et composer.json

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new Pokou\CenterBundle\PokouCenterBundle(),
             new Pokou\AdminBundle\PokouAdminBundle(),
             new Pokou\UserBundle\PokouUserBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
